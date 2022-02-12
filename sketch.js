c912599cb3ab03b161364ea7244b06eaadfbe769
Cameron <cc44@utas.edu.au>
2022-02-12 16:37:13 +1100

    improved cursor

--- sketch.js
+++ sketch.js
@@ -10,27 +10,29 @@ let colours = [
 ];
 let gameWon = false;
 let hintMsg;
+let colourPallete;
 let answer;
 let guesses = [];
 let results = [];
 let guessRound = 0;
 let colourCursor = 0;
-let debugging = false;
+let debug = false;
 
 function setup() {
   ellipseMode(CORNER);
   createCanvas(windowWidth, 1600);
 
-  hintMsg = new Message("Use number keys to choose a new colour", 50, 70);
+  hintMsg = new Message("Use number keys to choose a new colour", 50, 110);
+  colourPallete = new CodeGuess(20, [0, 1, 2, 3, 4, 5, 6, 7], false);
   answer = new CodeGuess(30, generateCode(), true);
-  guesses.push(new CodeGuess(30, [9, 9, 9, 9, 9], false));
+  guesses.push(new CodeGuess(30, ["", "", "", "", ""], false));
 }
 
 function draw() {
   background("#cccccc");
 
   // debugging
-  if (debugging) {
+  if (debug) {
     fill(50);
     stroke(0);
     text(
@@ -45,9 +47,8 @@ function draw() {
   // game text
   fill(50);
   stroke(0);
-  text("Welcome to Mastermind!", 20, 30);
-
-  hintMsg.show();
+  text("Welcome to Mastermind!", 50, 30);
+  colourPallete.show(50, 40);
 
   // guess area
   fill(50);
@@ -56,9 +57,14 @@ function draw() {
     fill(50);
     stroke(0);
     text(`Guess ${i + 1}:`, guesses[i].x, guesses[i].y - 5);
-    guesses[i].show(50, 100 + 100 * i);
+    if (i == guesses.length - 1) {
+      guesses[i].show(50, 150 + 100 * i, colourCursor);
+      hintMsg.show(50, 230 + 100 * i);
+    } else {
+      guesses[i].show(50, 150 + 100 * i);
+    }
     if (i != 0) {
-      results[i - 1].show(300, 100 * i);
+      results[i - 1].show(300, 50 + 100 * i);
     }
   }
   3;
@@ -69,34 +75,35 @@ function keyPressed() {
     if (key >= 0 && parseInt(key) <= 7) {
       //Update colour
       guesses[guessRound].makeGuess(colourCursor, parseInt(key));
-      hintMsg.updateMsg("Press enter to select colour");
+      hintMsg.updateMsg(
+        "Press SPACE to move cursor, then ENTER to submit guess"
+      );
+    }
+    if (keyCode === 32) {
+      //Selection confirmed (Space pressed)
+      if (colourCursor >= 4) {
+        colourCursor = 0;
+      } else {
+        //Move to next colour
+        colourCursor++;
+        hintMsg.updateMsg("Use number keys to choose a new colour");
+      }
+      hintMsg.updateMsg("Use number keys to choose a new colour");
+      return false;
     }
     if (keyCode === 13) {
-      //Selection confirmed (Enter pressed)
-      if (colourCursor === 4) {
-        //Make full guess
-        let result = compareCodes(answer.guess, guesses[guessRound].guess);
-        results.push(new CodeResult(20, result));
-        if (arrayEquals(result, [2, 2, 2, 2, 2])) {
-          //Game won!
-          console.log("Game won!");
-          gameWon = true;
-          hintMsg.updateMsg("Congratulations, you are a mastermind!");
-        } else {
-          //Cleanup for next round
-          colourCursor = 0;
-          guesses.push(new CodeGuess(30, [9, 9, 9, 9, 9], false));
-          guessRound++;
-        }
+      //Make full guess (Enter pressed)
+      let result = compareCodes(answer.guess, guesses[guessRound].guess);
+      results.push(new CodeResult(20, result));
+      if (arrayEquals(result, [2, 2, 2, 2, 2])) {
+        //Game won!
+        gameWon = true;
+        hintMsg.updateMsg("Congratulations, you are a mastermind!");
       } else {
-        if (!(guesses[guessRound].guess[colourCursor] === "x")) {
-          //Move to next colour
-          colourCursor++;
-          hintMsg.updateMsg("Use number keys to choose a new colour");
-        } else {
-          //Invalid selection
-          hintMsg.updateMsg("You need to choose a colour first");
-        }
+        //Cleanup for next round
+        colourCursor = 0;
+        guesses.push(new CodeGuess(30, ["", "", "", "", ""], false));
+        guessRound++;
       }
     }
   }
@@ -112,14 +119,11 @@ function generateCode() {
     } while (code.includes(randOpt));
     code[i] = randOpt;
   }
-  console.log("Colour code:", colours);
-  console.log("Answer:", code);
   return code;
 }
 
 function compareCodes(ans, guess) {
   let result = [];
-  console.log(ans, guess);
   for (let i = 0; i < ans.length; i++) {
     if (ans[i] === guess[i]) {
       result[i] = 2; //correct position
@@ -129,7 +133,6 @@ function compareCodes(ans, guess) {
       result[i] = 0; //incorrect
     }
   }
-  console.log(result);
   return result;
 }
 
@@ -156,7 +159,7 @@ class CodeGuess {
     this.guess[pos] = guess;
   }
 
-  show(x, y) {
+  show(x = this.x, y = this.y, cursor = undefined) {
     this.x = x;
     this.y = y;
 
@@ -169,37 +172,50 @@ class CodeGuess {
     rect(
       this.x,
       this.y,
-      this.padding + (this.padding + this.size) * 5,
+      this.padding + (this.padding + this.size) * this.guess.length,
       this.size + this.padding * 2
     );
 
     for (let i = 0; i < this.guess.length; i++) {
+      if (cursor !== undefined) {
+        stroke(0);
+        strokeWeight(3);
+        fill(0, 0, 0, 0);
+        ellipse(
+          this.x + this.padding + (this.padding + this.size) * cursor,
+          this.y + this.padding,
+          this.size,
+          this.size
+        );
+      }
+
       stroke(0);
-      if (this.guess[i] == 9) {
+      strokeWeight(1);
+      if (this.guess[i] == "") {
         stroke("#cccccc");
         fill("white");
       } else {
         fill(colours[this.guess[i]]);
       }
 
+      strokeWeight(1);
       ellipse(
         this.x + this.padding + (this.padding + this.size) * i,
         this.y + this.padding,
         this.size,
         this.size
       );
-      if (debugging) {
-        fill("#cccccc");
-        text(
-          this.guess[i],
-          this.x +
-            this.padding +
-            (this.padding + this.size) * i +
-            this.size / 2 -
-            3,
-          this.y + this.padding + this.size / 2 + 3
-        );
-      }
+
+      fill("#cccccc");
+      text(
+        this.guess[i],
+        this.x +
+          this.padding +
+          (this.padding + this.size) * i +
+          this.size / 2 -
+          3,
+        this.y + this.padding + this.size / 2 + 3
+      );
     }
   }
 }
@@ -213,7 +229,7 @@ class CodeResult {
     this.guess = guess;
   }
 
-  show(x, y) {
+  show(x = this.x, y = this.y) {
     this.x = x;
     this.y = y;
 
@@ -222,7 +238,7 @@ class CodeResult {
     rect(
       this.x,
       this.y,
-      this.padding + (this.padding + this.size) * 5,
+      this.padding + (this.padding + this.size) * this.guess.length,
       this.size + this.padding * 2
     );
 
@@ -264,7 +280,11 @@ class Message {
     this.msg = newMsg;
   }
 
-  show() {
+  show(x = this.x, y = this.y) {
+    this.x = x;
+    this.y = y;
+
+    fill(50);
     stroke("red");
     text(this.msg, this.x, this.y);
   }
